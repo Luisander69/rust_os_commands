2b7a36c35cf8d0abc56a0a030d250e025e9480f7
lui <stefaniukliubomyr@gmail.com>
2023-10-15 17:05:17 +0300

    added windows support

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,28 @@ use std::process::Command;
 use std::env;
 
 fn executecmd(cmd: &str) -> String{
-    let temp = "-c ".to_owned();
-    let fullcmd = temp + cmd;
-    let cmds: Vec<&str> = fullcmd.split(" ").collect();
-    let res = Command::new("sh").arg(&cmds[0]).arg(&cmds[1]).output().unwrap();
-    let stdout = String::from_utf8_lossy(res.stdout.as_slice());
-    let stderr = String::from_utf8_lossy(res.stderr.as_slice());
+    
+let res = if cfg!(target_os = "windows") {
+        let temp = "/C ".to_owned();
+        let fullcmd = temp + cmd;
+        let cmds: Vec<&str> = fullcmd.split(" ").collect();
+        Command::new("cmd").arg(&cmds[0]).arg(&cmds[1]).output().unwrap()
+} else {
+        let temp = "-c ".to_owned();
+        let fullcmd = temp + cmd;
+        let cmds: Vec<&str> = fullcmd.split(" ").collect();
+        Command::new("sh").arg(&cmds[0]).arg(&cmds[1]).output().unwrap()
+};
 
-    if stdout.len() > 0{
-        return stdout.to_string();
-    }else{
-        return stderr.to_string();
-    }
+        let stdout = String::from_utf8_lossy(res.stdout.as_slice());
+        let stderr = String::from_utf8_lossy(res.stderr.as_slice());
+
+        if stdout.len() > 0{
+            return stdout.to_string();
+        }else{
+            return stderr.to_string();
+        }
+
 }
 
 
