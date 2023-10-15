10b9e0d84833859bca46f1d72288fea29ac60337
luisander <stefaniukliubomyr@gmail.com>
2023-10-15 13:44:36 +0000

    finished app, works on linux only

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::process::Command;
+use std::env;
+
+fn executecmd(cmd: &str) -> String{
+    let temp = "-c ".to_owned();
+    let fullcmd = temp + cmd;
+    let cmds: Vec<&str> = fullcmd.split(" ").collect();
+    let res = Command::new("sh").arg(&cmds[0]).arg(&cmds[1]).output().unwrap();
+    let stdout = String::from_utf8_lossy(res.stdout.as_slice());
+    let stderr = String::from_utf8_lossy(res.stderr.as_slice());
+
+    if stdout.len() > 0{
+        return stdout.to_string();
+    }else{
+        return stderr.to_string();
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        let res = executecmd(&args[1]);
+        println!("{}", res);
+    }else{
+        println!("[+]Usage {} command", args[0]);
+    }
 }
